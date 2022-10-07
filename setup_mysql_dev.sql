674b402b26dbbff3e7ae6fbcaafd6765507091aa
YoussefJell <youssef.jellouli@yahoo.com>
2022-10-07 12:27:20 +0000

    Changed env vars

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS binomi_dev_db;
+CREATE USER IF NOT EXISTS 'binomi_dev'@'localhost' IDENTIFIED BY 'binomi_dev_pwd';
+GRANT ALL PRIVILEGES ON `binomi_dev_db`.* TO 'binomi_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'binomi_dev'@'localhost';
 FLUSH PRIVILEGES;
