b2166d1762ae0762f08990f7fdd559b0179e315d
YoussefJell <youssef.jellouli@yahoo.com>
2022-10-07 10:51:16 +0000

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """ Index """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+from models.preference import Preference
 from models.user import User
 from models import storage
 from api.v1.views import app_views
@@ -20,8 +16,8 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    classes = [Preference, User]
+    names = ["preferences", "users"]
 
     num_objs = {}
     for i in range(len(classes)):
